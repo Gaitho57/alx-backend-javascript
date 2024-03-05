e366fc7d11ca20cba75134f0ee569ea91cbb820a
Gaitho57 <michaelgaitho10@gmail.com>
2024-03-05 07:49:49 +0000

    update

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,9 @@
+export function taskFirst() {
+  const task1 = 'I prefer const when I can.';
+  return task1;
+}
+export function tasktaskNext() {
+  let task2 = 'But sometimes let is okay';
+  return task2;
+}
+console.log(`${taskFirst()} ${tasktaskNext()}`);
