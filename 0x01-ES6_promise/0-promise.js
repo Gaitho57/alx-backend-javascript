2ad810a96478c8bf11e2f3d384ad352066e106aa
Gaitho57 <michaelgaitho10@gmail.com>
2024-03-06 14:29:52 +0000

    promise update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,14 @@
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-setTimeout(() => {
-	resolve('promise resolved');
-}, 200);
-});
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('true');
+    }, 200);
+  });
 }
 getResponseFromAPI()
-.then(response => {
-	console.log(response);
-})
-.catch(error => {
-	console.log(error)
-})
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
