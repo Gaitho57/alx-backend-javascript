cfd98fb600dfd8cb45135e634432dd68a32656a7
Gaitho57 <michaelgaitho10@gmail.com>
2024-03-07 05:40:28 +0000

    updatete

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,12 @@
 function handleResponseFromAPI(promise) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve({status: 200, body: success});
-			reject(new Error());
-		},2000});
+  return (promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return ({ status: 200, body: 'success' });
+    })
+    .catch((error) => { return(new Error());
+    })
+  );
 }
-handleResponseFromAPI(promise)
 
-.then((response) => {
-	console.log(response);
-}
-.catch ((error) => {
-	console.log('Rejected:', error); // Logging the empty Error object
-  });
-export default handleResponseFromAPI(promise);
+export default handleResponseFromAPI;
