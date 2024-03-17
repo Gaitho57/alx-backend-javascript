9da33721474eafc42b4cdd6da4f4722b71ca6e57
Gaitho57 <michaelgaitho10@gmail.com>
2024-03-17 11:18:57 +0000

    update

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,10 +2,40 @@ export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
-    this._color = color;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
 }
