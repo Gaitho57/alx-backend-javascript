5e4c8b0c158c277eace8d89288f73c230783514f
Gaitho57 <michaelgaitho10@gmail.com>
2024-05-04 16:28:54 +0300

    api

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,28 +1,41 @@
-const request = require("request");
-const {describe, it} = require("mocha");
-const expect = require("chai").expect;
+const assert = require('assert');
+const request = require('request');
+const app = require('./api');
 
-describe("Index page", function() {
-    const options = {
-	url: "http://localhost:7865/",
-	method: "GET"
-    }
-    it("check correct status code", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-	});
+const baseUrl = 'http://localhost:7865';
+
+describe('Index page', () => {
+  // Existing tests for the index page
+  // ...
+
+  // New test suite for the cart page
+  describe('Cart page', () => {
+    it('should return correct status code when :id is a number', (done) => {
+      request.get(`${baseUrl}/cart/123`, (error, response) => {
+        assert.strictEqual(response.statusCode, 200);
+        done();
+      });
     });
-    it("check correct content", function(done) {
-	request(options, function(err, res, body) {
-	    expect(body).to.contain("Welcome to the payment system");
-	    done();
-	});
+
+    it('should return correct result when :id is a number', (done) => {
+      request.get(`${baseUrl}/cart/456`, (error, response, body) => {
+        assert.strictEqual(body, 'Payment methods for cart 456');
+        done();
+      });
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.headers['content-length']).to.equal('29');
-	    done();
-	});
+
+    it('should return 404 status code when :id is NOT a number', (done) => {
+      request.get(`${baseUrl}/cart/abc`, (error, response) => {
+        assert.strictEqual(response.statusCode, 404);
+        done();
+      });
     });
+
+    // Add more test cases as needed
+  });
+});
+
+// Close the server after all tests are done
+after(() => {
+  app.close();
 });
